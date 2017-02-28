7c48430471164176575e804542034792b6caf0da
Anton Parkhomenko <anton380@gmail.com>
2017-02-28 14:43:31 +0200

    mobile enhancements

--- export/scripts.js
+++ export/scripts.js
@@ -2,6 +2,8 @@
 
     if(window.location.hash == '#edit-content') {
         document.body.contentEditable = true
+        document.querySelector('.edit-content').style.display = 'block';
+
     }
 
 })();
