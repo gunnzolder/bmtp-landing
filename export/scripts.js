10b97a8f853ef43aa3880ed76b762a3db9bb4aab
Anton Parkhomenko <anton380@gmail.com>
2017-06-09 11:38:56 +0300

    email templates added; latest changes

--- export/scripts.js
+++ export/scripts.js
@@ -1,10 +1,13 @@
 (function($){
 
-    var date = '2017/06/05';
+    var date = '2017/06/26';
+    var days = (window.location.pathname.indexOf('EN') > -1) ? 'days' : 'dage' ;
+
 
     $('#clock').countdown(date, function(event) {
-        $(this).html(event.strftime('%D dage %H:%M:%S'));
+        $(this).html(event.strftime('%D ' + days + ' %H:%M:%S'));
     });
+
 })($);
 (function(){
 
